b3d7f23ba60b7d148f1b0a84a79bbeefd745e4f8
Veys101 <vys2626@gmail.com>
2023-03-22 00:13:49 +0300

    set and map added

--- groovyDemo/app/src/main/groovy/com/app/demo/CollectionsDemo.groovy
+++ groovyDemo/app/src/main/groovy/com/app/demo/CollectionsDemo.groovy
@@ -24,5 +24,25 @@ class CollectionsDemo {
         l.reverseEach {print it}
         println ""
         l.eachPermutation{println it}
+
+        def s = ['java', 'js', 'python', 'js'] as Set 
+        println s 
+        println s.class
+
+        def m = [courseName:'Gradle',rating:5,price:10]
+        println m
+
+        m.each {
+            k,v->
+            println("${k} -- ${v}")
+        }
+
+        println m.courseName
+        println m['courseName']
+        println m.get('courseName')
+
+        m.review = 'its awesome'
+
+        println m.get('review')
     }
 }
